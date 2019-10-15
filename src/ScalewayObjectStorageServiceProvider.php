49a6d2e44925eedc7af25e453909627293c40cc8
superDuperCyberTechno <superDuperCyberTechno@users.noreply.github.com>
2019-10-15 12:48:25 +0200

    Update ScalewayObjectStorageServiceProvider.php

--- src/ScalewayObjectStorageServiceProvider.php
+++ src/ScalewayObjectStorageServiceProvider.php
@@ -4,9 +4,9 @@ namespace superDuperCyberTechno\Crawford;
 
 use Storage;
 use Aws\S3\S3Client;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
 
 class ScalewayObjectStorageServiceProvider extends ServiceProvider{
     public function register(){
@@ -15,8 +15,10 @@ class ScalewayObjectStorageServiceProvider extends ServiceProvider{
 
     public function boot()
     {
+        //extend the storage class and add the driver
         Storage::extend('scaleway', function ($app, $config) {
             $client = new S3Client([
+                //pass the client config directly into the constructor
                 'credentials' => [
                     'key'    => $config['key'],
                     'secret' => $config['secret'],
@@ -26,6 +28,7 @@ class ScalewayObjectStorageServiceProvider extends ServiceProvider{
                 'endpoint' => 'https://s3.' . $config['region'] . '.scw.cloud',
             ]);
 
+            //create adapter and return the filesystem var
             $adapter = new AwsS3Adapter($client, $config['bucket']);
             $filesystem = new Filesystem($adapter);
 
